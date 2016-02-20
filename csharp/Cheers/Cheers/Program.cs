925c4c76d36e74aed67c10e25c824a07dad6b50f
jonathanhuffaker <jonathanhuffaker@gmail.com>
2016-02-20 09:42:10 -0800

    pushing what I have so far

--- csharp/Cheers/Cheers/Program.cs
+++ csharp/Cheers/Cheers/Program.cs
@@ -10,6 +10,27 @@ namespace Cheers
     {
         static void Main(string[] args)
         {
+            var vowel = new[] { "a", "e", "i", "o", "u" };
+            
+            Console.Clear();
+            string name;
+            Console.WriteLine("\nWhat's your name?");
+            Console.WriteLine();
+            do
+            {
+                Console.Write("  ");
+                name = Console.ReadLine();
+                if (name != null)
+                    Console.WriteLine("Give me a(n) ..."+ (name[0]));
+                       //Assert.Equal(name, name[0]);
+                    Console.WriteLine("Give me a(n) ...  " + (name[1]));
+                    Console.WriteLine("Give me a(n)..." + (name[2]));
+
+                    Console.WriteLine( name + " is .. GRAND!");
+
+            } while (name != null);
+
+             
         }
     }
 }
