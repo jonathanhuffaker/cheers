f04b37479add35724db82b48e3c7accb1e422de3
jonathanhuffaker <jonathanhuffaker@gmail.com>
2016-02-28 17:14:49 -0800

    Still working on subtracting days

--- csharp/Cheers/Cheers/Program.cs
+++ csharp/Cheers/Cheers/Program.cs
@@ -16,24 +16,35 @@ namespace Cheers
             string name;
             string bday;
             Console.WriteLine("What's your name?");
-            Console.WriteLine();
+            name = Console.ReadLine();
+
+            Console.WriteLine("What's your birthday (mm/dd)?");
+            bday = Console.ReadLine();
+
+            DateTime today = DateTime.Now;
+            DateTime bdaydate = Convert.ToDateTime(bday);
+            var daysTill = (today - bdaydate).Days;
+            Console.WriteLine(name);
+            Console.WriteLine(bday);
+            Console.WriteLine(daysTill);
+/*
             do
             {
                 Console.Write("  ");
                 name = Console.ReadLine();
-               if (name != null)
+               if (name != null) */
                     for (int i = 0; i < name.Length; i++) {
                         Console.WriteLine("Give me a(n) ..." + (name[i]));
                        
-                    }
+                    } 
                 //Assert.Equal(name, name[0]);
                 //Console.WriteLine("Give me a(n) ...  " + (name[1]));
                 //Console.WriteLine("Give me a(n)..." + (name[2]));
 
                 Console.WriteLine( name + " is .. GRAND!");
 
-            } while (name != null);
-
+            //} while (name != null);
+            Console.ReadLine();
              
         }
     }
